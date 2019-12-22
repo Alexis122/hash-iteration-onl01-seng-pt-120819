eabf21f19a00a85e0bab9561d92d9b98e580daef
Alexis Priore <alexispriore@gmail.com>
2019-12-22 20:24:30 +0000

    Automatically backed up by Learn

--- spec/01_cruise_ship_spec.rb
+++ spec/01_cruise_ship_spec.rb
@@ -13,6 +13,8 @@ describe "#select_winner" do
       }
 #
 #
+xpect(select_winner(passengers)).to eq("Amanda Presley")
+
   winner = " "
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
